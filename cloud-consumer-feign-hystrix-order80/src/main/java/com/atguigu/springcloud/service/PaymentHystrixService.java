4432c94d0083377978a979af371c49103f38b957
ywj <2435432539@qqcom>
2020-07-02 17:50:54 +0800

    cloud2020

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author Ywj
+ * @version 1.0
+ * @date 2020/7/2 14:45
+ */
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService {
+
+    @GetMapping(value = "/payment/hystrix/ok/{id}")
+    public String paymentInfo_ok(@PathVariable("id") Integer id);
+
+    @GetMapping(value = "/payment/hystrix/timeout/{id}")
+    public String paymentInfo_timeout(@PathVariable("id") Integer id);
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -0,0 +1,37 @@
+package com.atguigu.springcloud.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Ywj
+ * @version 1.0
+ * @date 2020/7/2 10:50
+ */
+@Service
+public class PaymentService {
+
+    //成功
+    public String paymentInfo_OK(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_OK,id：  "+id+"\t"+"哈哈哈"  ;
+    }
+
+    //失败  @HystrixCommand系统运行异常或者超时兜底方法
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties ={
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")
+    })
+    public String paymentInfo_TimeOut(Integer id){
+        int timeNumber = 3;
+        try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
+        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+
+        return "线程池："+Thread.currentThread().getName()+"   系统繁忙，请稍后再试,id：  "+id+"\t"+".........";
+    }
+
+}
